ffdf1a968fadff194ecd1e7d3c0fa87976059c7f
Phil Fitch <mostreluctantly@gmail.com>
2019-11-08 17:20:15 -0800

    a little styling

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../styles/results.css';
 
 const Results = ({ json }) => {
   return (
     <>
-      <h1>RESULTS</h1>
+      <h2>RESULTS</h2>
       <pre>{json}</pre>
     </>
   );
--- src/components/form/UrlInputForm.js
+++ src/components/form/UrlInputForm.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../../styles/url-input-form.css';
 
 const UrlInputForm = ({ handleUrlChange, handleMethodChange, handleInputJsonChange, handleSubmit }) => {
 
   return (
     <>
-      <h1>FORM</h1>
+      <h2>FORM</h2>
       <form onSubmit={handleSubmit}>
-        <input type='text' placeholder='url' onChange={handleUrlChange} />
-        <input type='radio' name='method' value='get' onChange={handleMethodChange} /> GET
-        <input type='radio' name='method' value='post' onChange={handleMethodChange} /> POST
-        <input type='radio' name='method' value='put' onChange={handleMethodChange} /> PUT
-        <input type='radio' name='method' value='delete' onChange={handleMethodChange} /> DELETE
+        <div>
+          <input id='url_input' type='text' placeholder='url' onChange={handleUrlChange} />
+          <input type='radio' name='method' value='get' onChange={handleMethodChange} /> GET
+          <input type='radio' name='method' value='post' onChange={handleMethodChange} /> POST
+          <input type='radio' name='method' value='put' onChange={handleMethodChange} /> PUT
+          <input type='radio' name='method' value='delete' onChange={handleMethodChange} /> DELETE
+        </div>
         <textarea placeholder='json' onChange={handleInputJsonChange} />
         <button>Submit</button>
       </form>
